271d85298b7401337cecc4316057138b675142a6
Seona Bellamy <seona13@gmail.com>
2023-02-21 14:15:14 +1100

    Character Visual and Rotation
    - Replace the basic capsule with the final character art, and have the character rotate to face the direction it's moving.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -32,5 +32,8 @@ public class Player : MonoBehaviour
         inputVector = inputVector.normalized;
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, rotateSpeed * Time.deltaTime);
     }
 }
