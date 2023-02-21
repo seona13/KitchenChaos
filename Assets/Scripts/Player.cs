ed1a86d8fb50907f3ebd36b73bbb0f829ef8589d
Seona Bellamy <seona13@gmail.com>
2023-02-21 17:17:09 +1100

    Animations
    - Animating the player, both idle and walking states.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,8 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float moveSpeed = 7f;
 
+    private bool isWalking;
+
 
     private void Update()
     {
@@ -33,7 +35,15 @@ public class Player : MonoBehaviour
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
 
+        isWalking = moveDir != Vector3.zero;
+
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, rotateSpeed * Time.deltaTime);
+    }
+
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
