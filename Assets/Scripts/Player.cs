b6bdbc2e377e4930685a64f8e1807a45a89a2bb0
Seona Bellamy <seona13@gmail.com>
2023-05-10 08:11:30 +1000

    Clear Counter
    Created the Clear Counter game object and turned it into a prefab.
    Used a raycast from the player to detect when the object in front of them is a counter (using a layermask for extra insurance).
    Automatically interact with the counter when the player approaches it.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,11 +9,49 @@ public class Player : MonoBehaviour
     private float moveSpeed = 7f;
     [SerializeField]
     private GameInput gameInput;
+    [SerializeField]
+    private LayerMask countersLayerMask;
 
     private bool isWalking;
+    private Vector3 lastInteractDir;
 
 
     private void Update()
+    {
+        HandleMovement();
+        HandleInteractions();
+    }
+
+
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+
+
+    private void HandleInteractions()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        if (moveDir != Vector3.zero)
+        {
+            lastInteractDir = moveDir;
+        }
+
+        float interactDistance = 2f;
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                // Has ClearCounter
+                clearCounter.Interact();
+            }
+        }
+    }
+
+
+    private void HandleMovement()
     {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
@@ -32,7 +70,7 @@ public class Player : MonoBehaviour
             // Attempt only X movement
             Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
             canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
-        
+
             if (canMove)
             {
                 // Can move only on the X
@@ -63,11 +101,5 @@ public class Player : MonoBehaviour
 
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, rotateSpeed * Time.deltaTime);
-    }
-
-
-    public bool IsWalking()
-    {
-        return isWalking;
     }
 }
