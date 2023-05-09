305beeffd8f43b9d064c804e37a5c78bdc9d6538
Seona Bellamy <seona13@gmail.com>
2023-05-10 09:17:13 +1000

    Interact Action
    - Counter interaction now happens on a keypress, rather than just by walking up to the counter.

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -1,10 +1,11 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class GameInput : MonoBehaviour
 {
+    public event EventHandler OnInteractAction;
+
     private PlayerInputActions playerInputActions;
 
 
@@ -12,6 +13,14 @@ public class GameInput : MonoBehaviour
     {
         playerInputActions = new PlayerInputActions();
         playerInputActions.Player.Enable();
+
+        playerInputActions.Player.Interact.performed += Interact_performed;
+    }
+
+
+    private void Interact_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        OnInteractAction?.Invoke(this, EventArgs.Empty);
     }
 
 
