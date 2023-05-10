b3137e3b72c1e70dbe39a3955ff840f2510c7e0c
Seona Bellamy <seona13@gmail.com>
2023-05-11 06:49:41 +1000

    Kitchen Objects
    - Created prefabs for first two kitchen objects.
    - Created scriptable object to hold kitchen object data.
    - Counters now spawn kitchen objects when interacted with.

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+    [SerializeField]
+    private Transform counterTopPoint;
+    [SerializeField]
+    private KitchenObjectSO kitchenObjectSO;
+
+
     public void Interact()
     {
         Debug.Log("Interact");
+        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+        kitchenObjectTransform.localPosition = Vector3.zero;
+
+        kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO();
     }
 }
